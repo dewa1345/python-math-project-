5dda94923fdd6c8dd7b2e6d1c67f02235dcbbaf8
A.Dewa Fortuna M <dewafortunamogot@gmail.com>
2025-01-17 15:01:18 +0800

    ready to lunch app 1.0

---
+++ main.py
@@ -0,0 +1,37 @@
+from fitur.sum import calculate_sum
+from fitur.product import calculate_product
+from fitur.div import div
+from fitur.difference import calculate_difference
+
+a = 10
+b = 5
+sum_result = calculate_sum(a, b)
+print(sum_result)
+
+sum_result = calculate_product(a, b)
+print(sum_result)
+
+sum_result = div(a, b)
+print(sum_result)
+
+sum_result = calculate_difference(a, b)
+print(sum_result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
